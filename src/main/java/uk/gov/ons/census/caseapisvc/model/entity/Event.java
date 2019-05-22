9b8c3969ef103bcd36deaf093240477ca7982ca8
AdeHarrison <adrian.harrison.uk@gmail.com>
2019-05-22 10:57:30 +0100

    updated event domain from case-processor with eventType changes

--- src/main/java/uk/gov/ons/census/caseapisvc/model/entity/Event.java
+++ src/main/java/uk/gov/ons/census/caseapisvc/model/entity/Event.java
@@ -4,6 +4,8 @@ import java.util.Date;
 import java.util.UUID;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Transient;
@@ -17,10 +19,11 @@ public class Event {
 
   @ManyToOne private UacQidLink uacQidLink;
 
-  // TODO remove @Transient when field in schema
-  @Column @Transient private EventType eventType;
-
   @Column private Date eventDate;
 
   @Column private String eventDescription;
+
+  @Column
+  @Enumerated(EnumType.STRING)
+  private EventType eventType;
 }
