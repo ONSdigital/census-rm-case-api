7e51e59a66f4790ed474e6132398481943ff2fed
Nick Grant <nick.grant@ext.ons.gov.uk>
2019-06-04 09:24:11 +0100

    Force UTC timezone for belt-and-braces

--- README.md
+++ README.md
@@ -5,7 +5,5 @@ This service provides a read-only API for case details.
 The Case Api service requires a Postgres instance to be running which contains the new casev2 schema.
 Postgres can be started using either census-rm-docker-dev or "docker-compose up -d postgres-database".
 
-If you want to run on your local machine in a different timezone from UTC, you can force UTC by specifying `-Duser.timezone=UTC` as a JVM option.
-
 # How to test
 make test
--- src/main/java/uk/gov/ons/census/caseapisvc/config/AppConfig.java
+++ src/main/java/uk/gov/ons/census/caseapisvc/config/AppConfig.java
@@ -3,6 +3,8 @@ package uk.gov.ons.census.caseapisvc.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import ma.glasnost.orika.MapperFacade;
 import ma.glasnost.orika.MapperFactory;
 import ma.glasnost.orika.impl.DefaultMapperFactory;
@@ -24,5 +26,10 @@ public class AppConfig {
     objectMapper.registerModule(new JavaTimeModule());
     objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     return objectMapper;
+  }
+
+  @PostConstruct
+  public void init(){
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
   }
 }
