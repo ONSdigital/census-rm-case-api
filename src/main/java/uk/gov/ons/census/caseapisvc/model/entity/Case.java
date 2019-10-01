b5bd86eeca8ccc2d1e7df28de8b7a7ee5ca9a175
AdeHarrison <adrian.harrison.uk@gmail.com>
2019-10-01 11:27:29 +0100

    update model objects to include new ccsCase property

--- src/main/java/uk/gov/ons/census/caseapisvc/model/entity/Case.java
+++ src/main/java/uk/gov/ons/census/caseapisvc/model/entity/Case.java
@@ -103,4 +103,7 @@ public class Case {
 
   @Column(nullable = false, columnDefinition = "BOOLEAN DEFAULT false")
   private boolean addressInvalid;
+
+  @Column(nullable = false, columnDefinition = "BOOLEAN DEFAULT false")
+  private boolean ccsCase;
 }
