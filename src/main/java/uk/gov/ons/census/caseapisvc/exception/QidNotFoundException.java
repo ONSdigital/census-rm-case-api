88f9eb999ae7d555833c394ecc3c8dff0de1ccad
AdamHawtin <adam.hawtin@ons.gov.uk>
2019-10-08 10:55:46 +0100

    Format

--- src/main/java/uk/gov/ons/census/caseapisvc/exception/QidNotFoundException.java
+++ src/main/java/uk/gov/ons/census/caseapisvc/exception/QidNotFoundException.java
@@ -6,6 +6,7 @@ public class QidNotFoundException extends RuntimeException {
   public QidNotFoundException(String qid) {
     super(String.format("Qid Not Found '%s' not found", qid));
   }
+
   public QidNotFoundException(UUID caseId) {
     super(String.format("Qid Not Found for case ID: '%s'", caseId));
   }
