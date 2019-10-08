d85afcb5fe20dbede007d6ecd3530abea8127688
Adam <35268982+AdamHawtin@users.noreply.github.com>
2019-10-08 12:21:59 +0100

    Add constructor for QidNotFoundException with caseId (#34)

--- src/main/java/uk/gov/ons/census/caseapisvc/exception/QidNotFoundException.java
+++ src/main/java/uk/gov/ons/census/caseapisvc/exception/QidNotFoundException.java
@@ -1,7 +1,13 @@
 package uk.gov.ons.census.caseapisvc.exception;
 
+import java.util.UUID;
+
 public class QidNotFoundException extends RuntimeException {
   public QidNotFoundException(String qid) {
     super(String.format("Qid Not Found '%s' not found", qid));
+  }
+
+  public QidNotFoundException(UUID caseId) {
+    super(String.format("Qid Not Found for case ID: '%s'", caseId));
   }
 }
