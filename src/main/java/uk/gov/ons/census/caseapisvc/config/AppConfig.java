95beea372a1bced030f7592c086474bbb7009f66
Nick Grant <nick.grant@ext.ons.gov.uk>
2020-05-20 16:10:19 +0100

    Update AppConfig.java

--- src/main/java/uk/gov/ons/census/caseapisvc/config/AppConfig.java
+++ src/main/java/uk/gov/ons/census/caseapisvc/config/AppConfig.java
@@ -65,4 +65,5 @@ public class AppConfig {
   @Bean
   StackdriverMeterRegistry meterRegistry(StackdriverConfig stackdriverConfig) {
     return StackdriverMeterRegistry.builder(stackdriverConfig).build();
-  }}
+  }
+}
