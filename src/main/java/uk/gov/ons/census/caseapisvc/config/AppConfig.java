271e7abd7b5f7a6050581b0172ec26c698b3d751
Nick Grant <41681172+NickGrantONS@users.noreply.github.com>
2020-05-22 12:35:27 +0100

    Push metrics into Stackdriver (#79)

--- src/main/java/uk/gov/ons/census/caseapisvc/config/AppConfig.java
+++ src/main/java/uk/gov/ons/census/caseapisvc/config/AppConfig.java
@@ -3,6 +3,12 @@ package uk.gov.ons.census.caseapisvc.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import io.micrometer.core.instrument.Meter;
+import io.micrometer.core.instrument.config.MeterFilter;
+import io.micrometer.core.instrument.config.MeterFilterReply;
+import io.micrometer.stackdriver.StackdriverConfig;
+import io.micrometer.stackdriver.StackdriverMeterRegistry;
+import java.time.Duration;
 import java.util.TimeZone;
 import javax.annotation.PostConstruct;
 import ma.glasnost.orika.MapperFacade;
@@ -11,11 +17,21 @@ import ma.glasnost.orika.impl.DefaultMapperFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+  @Value("${management.metrics.export.stackdriver.project-id}")
+  private String stackdriverProjectId;
+
+  @Value("${management.metrics.export.stackdriver.enabled}")
+  private boolean stackdriverEnabled;
+
+  @Value("${management.metrics.export.stackdriver.step}")
+  private String stackdriverStep;
+
   @Bean
   public MapperFacade mapperFacade() {
     MapperFactory mapperFactory = new DefaultMapperFactory.Builder().build();
@@ -43,5 +59,48 @@ public class AppConfig {
     objectMapper.registerModule(new JavaTimeModule());
     objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     return new Jackson2JsonMessageConverter(objectMapper);
+  }
+
+  @Bean
+  StackdriverConfig stackdriverConfig() {
+    return new StackdriverConfig() {
+      @Override
+      public Duration step() {
+        return Duration.parse(stackdriverStep);
+      }
+
+      @Override
+      public boolean enabled() {
+        return stackdriverEnabled;
+      }
+
+      @Override
+      public String projectId() {
+        return stackdriverProjectId;
+      }
+
+      @Override
+      public String get(String key) {
+        return null;
+      }
+    };
+  }
+
+  @Bean
+  public MeterFilter meterFilter() {
+    return new MeterFilter() {
+      @Override
+      public MeterFilterReply accept(Meter.Id id) {
+        if (id.getName().startsWith("rabbitmq")) {
+          return MeterFilterReply.DENY;
+        }
+        return MeterFilterReply.NEUTRAL;
+      }
+    };
+  }
+
+  @Bean
+  StackdriverMeterRegistry meterRegistry(StackdriverConfig stackdriverConfig) {
+    return StackdriverMeterRegistry.builder(stackdriverConfig).build();
   }
 }
