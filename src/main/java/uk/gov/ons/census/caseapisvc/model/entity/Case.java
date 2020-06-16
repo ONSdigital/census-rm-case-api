a13f09beeacf9a9ea8d6579825e74e627ed9ed9b
LukeLoze1 <44841627+LukeLoze1@users.noreply.github.com>
2020-06-16 07:25:21 +0100

    added surveyLaunched to case entity (#84)

--- src/main/java/uk/gov/ons/census/caseapisvc/model/entity/Case.java
+++ src/main/java/uk/gov/ons/census/caseapisvc/model/entity/Case.java
@@ -137,4 +137,7 @@ public class Case {
   private CaseMetadata metadata;
 
   @Column private String printBatch;
+
+  @Column(nullable = false, columnDefinition = "BOOLEAN DEFAULT false")
+  private boolean surveyLaunched;
 }
