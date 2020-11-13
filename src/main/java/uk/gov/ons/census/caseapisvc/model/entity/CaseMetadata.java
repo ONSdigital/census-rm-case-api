1fa7fe1ca355563e84868fdf72790e8474a899a0
Luke Loze <Luke.Loze@ext.ons.gov.uk>
2020-11-13 16:13:57 +0000

    Add NonCompliance Model

--- src/main/java/uk/gov/ons/census/caseapisvc/model/entity/CaseMetadata.java
+++ src/main/java/uk/gov/ons/census/caseapisvc/model/entity/CaseMetadata.java
@@ -1,10 +1,12 @@
 package uk.gov.ons.census.caseapisvc.model.entity;
 
 import lombok.Data;
+import uk.gov.ons.census.caseapisvc.model.dto.NonCompliancelTypeDTO;
 
 @Data
 public class CaseMetadata {
 
   private Boolean secureEstablishment;
   private String channel;
+  private NonCompliancelTypeDTO nonCompliance;
 }
