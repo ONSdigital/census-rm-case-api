b050facfed5d3d5a034eca3f5e6709155d0a2c4b
Luke Loze <Luke.Loze@ext.ons.gov.uk>
2020-11-17 14:01:35 +0000

    rename DTO

--- src/main/java/uk/gov/ons/census/caseapisvc/model/dto/NonComplianceTypeDTO.java
+++ src/main/java/uk/gov/ons/census/caseapisvc/model/dto/NonComplianceTypeDTO.java
@@ -1,6 +1,6 @@
 package uk.gov.ons.census.caseapisvc.model.dto;
 
-public enum NonCompliancelTypeDTO {
+public enum NonComplianceTypeDTO {
   NCL,
   NCF
 }
--- src/main/java/uk/gov/ons/census/caseapisvc/model/entity/CaseMetadata.java
+++ src/main/java/uk/gov/ons/census/caseapisvc/model/entity/CaseMetadata.java
@@ -1,12 +1,12 @@
 package uk.gov.ons.census.caseapisvc.model.entity;
 
 import lombok.Data;
-import uk.gov.ons.census.caseapisvc.model.dto.NonCompliancelTypeDTO;
+import uk.gov.ons.census.caseapisvc.model.dto.NonComplianceTypeDTO;
 
 @Data
 public class CaseMetadata {
 
   private Boolean secureEstablishment;
   private String channel;
-  private NonCompliancelTypeDTO nonCompliance;
+  private NonComplianceTypeDTO nonCompliance;
 }
